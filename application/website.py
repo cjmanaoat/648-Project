84d121044cc3a7c6b3481d7c2a17e79b79eb6aaa
cjmanaoat <cmanaoat@hotmail.com>
2020-10-19 18:31:30 -0700

    edited directory structure for about files(now stored in aboutHome folder). Fixed redirecting of about pages accordingly. Adjusted layout of navbar. Modified search filter dropdown

--- application/website.py
+++ application/website.py
@@ -1,6 +1,13 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+
+# mysql = MySQL()
+# mysql.init_app(app)
+
+# conn = mysql.connect()
+# cursor = conn.cursor()
 
 @app.route("/")
 def home():
@@ -8,11 +15,30 @@ def home():
 
 @app.route("/aboutHome")
 def aboutHome():
-    return render_template("aboutHome.html")
+    return render_template("/aboutHome/aboutHome.html")
 
-@app.route("/<aboutName>")
+@app.route("/aboutHome/<aboutName>")
 def aboutPage(aboutName):
-    return render_template({aboutName})
+    print(aboutName)
+    url = "/aboutHome/"+aboutName
+    print(url)
+    return render_template({url})
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    # if request.method == "POST":
+        # book = request.form['book']
+        # # search by author or book
+        # cursor.execute("SELECT name, author from Book WHERE name LIKE %s OR author LIKE %s", (book, book))
+        # conn.commit()
+        # data = cursor.fetchall()
+        # # all in the search box will return all the tuples
+        # if len(data) == 0 and book == 'all': 
+        #     cursor.execute("SELECT name, author from Book")
+        #     conn.commit()
+        #     data = cursor.fetchall()
+        # return render_template('search.html', data=data)
+    return render_template('search.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
